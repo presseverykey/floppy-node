b399641ae459bbcae7610fc878a62648038ebf7e
Tim Becker <tim@press-any-key.de>
2015-09-18 10:49:43 +0000

    start script

---
+++ raspbpi_auto_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NODE_PATH=/home/pi/node_setup/v0.10.24/node-v0.10.24-linux-arm-armv6j-vfp-hard/bin
+SCRIPT_PATH=/home/pi/node-midi-websocket
+LOG_PATH=/home/pi/logs
+
+echo "Starting..." >> ${LOG_PATH}/floppy.log
+date >> ${LOG_PATH}/floppy.log
+${NODE_PATH}/node server.js >> ${LOG_PATH}/floppy.log 2>&1 &
+echo $! >> ${LOG_PATH}/node.pid
